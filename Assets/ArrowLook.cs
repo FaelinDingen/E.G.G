2dd45e76f4cf04e6984274bc1362a39b77298add
Vigo-l <v.linders@sintlucasedu.nl>
2025-04-14 14:16:37 +0200

    cheeky pointer

---
+++ Assets/ArrowLook.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ArrowLook : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_Target;
+    public Transform LookAtTarget { get { return m_Target; } }
+
+    [SerializeField]
+    private Transform m_Spinner;
+    public Transform Spinner { get { return m_Spinner; } }
+    [SerializeField]
+    private Transform m_Scaler;
+    public Transform Scaler { get { return m_Scaler; } }
+
+    public float speed;
+
+    public void SetTarget(Transform target)
+    {
+        m_Target = target;
+
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (LookAtTarget)
+            transform.LookAt(LookAtTarget);
+
+        if (Spinner)
+            Spinner.transform.Rotate(0, 0, speed * Time.deltaTime);
+    }
+
+}
