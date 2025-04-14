1b118d07e875c885468b84324034adc89f83490e
FaelinDingen <127936791+FaelinDingen@users.noreply.github.com>
2025-04-14 11:48:17 +0200

    kleine update

--- Assets/Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,14 +1,30 @@
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10f;
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float jumpForce = 10f;
+
+    private Rigidbody rb;
+    private bool grounded;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        GetComponent<Rigidbody>().AddForce(movement * speed);
+        rb.AddForce(movement * speed);
+
+        //if (Physics.Raycast(gameObject.transform.position, Vector3.down))
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(new Vector3(0,jumpForce,0));
+        }
     }
 }
