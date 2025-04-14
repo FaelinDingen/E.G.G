1592f0381404e1b732548613c3869dc280ab1d30
FaelinDingen <127936791+FaelinDingen@users.noreply.github.com>
2025-04-14 15:26:21 +0200

    made whisk

---
+++ Assets/Scripts/EnemyAi/Whisk.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Whisk : MonoBehaviour
+{
+    private Singleton singleton;
+    private GameObject player;
+    private Rigidbody rb;
+
+    [SerializeField] private float whiskSpeed;
+
+    private void Awake() {
+        singleton = GameObject.Find("Singleton").GetComponent<Singleton>();
+        player = singleton.playerMovement.gameObject;
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        Vector3 direction = (player.transform.position - gameObject.transform.position).normalized;
+        rb.AddForce(direction * whiskSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            PlayerMovement playerMovement = player.GetComponent<PlayerMovement>();
+            Debug.Log("PlayerDied");
+        }
+    }
+}
